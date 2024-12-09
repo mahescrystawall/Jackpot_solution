3ad302f4b838af146a69f45a303211d2503829db
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-09 10:51:18 +0400

    menu and event api implementaion

--- jackpot_api/routes/api.php
+++ jackpot_api/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BetController;
 use App\Http\Controllers\Api\PriceValueController;
+use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\EventController;
+
 
 // Example of a route with authentication
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -15,3 +18,8 @@ Route::get('/unsettled_bets', [BetController::class, 'getUnsettledBets'])->name(
 
 // Route for fetching stakes value
 Route::get('/stakes', [PriceValueController::class, 'getStakes']);
+
+// Route for fetching menu items
+Route::get('/menu', [MenuController::class, 'getMenu']);
+
+Route::get('/events', [EventController::class, 'getEvents']);
